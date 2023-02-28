7227292888dafb53e967ce51767beab6123a7605
Pierrick Guillaume <pguillaume@sequans.com>
2023-02-28 14:41:26 +0100

    Update .config/fish/config.fish Remove .config/fish/functions/cm.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,7 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-abbr -a tm tmux
 abbr -a dc docker compose
 abbr -a ls exa -lh
 abbr -a vim nvim
@@ -11,3 +10,6 @@ abbr -a cm chezmoi
 abbr -a g git
 abbr -a ga git add
 abbr -a gc git commit
+
+abbr -a tm tmux
+abbr -a ta tmux attach -t
