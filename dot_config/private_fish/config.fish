b98af725274d8162dc04b71c7c06e6a0525d3a61
Pierrick Guillaume <pguillaume@sequans.com>
2023-03-07 08:07:40 +0100

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Add command abbreviation
 abbr -a dc docker compose
 abbr -a ls exa -lh
 abbr -a vim nvim
