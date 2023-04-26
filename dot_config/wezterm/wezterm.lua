c6516772e3f6f27f0c927918cf65e5c7d579ea7d
Pierrick Guillaume <pierguill@gmail.com>
2023-04-26 23:44:24 +0200

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,8 +1,23 @@
-local wt = require'wezterm'
+local wt = require 'wezterm'
 
 local config = wt.config_builder()
 
 config.color_scheme = 'Catppuccin Mocha'
 config.font = wt.font 'Cascadia Code'
+config.alternate_buffer_wheel_scroll_speed = 2
+config.animation_fps = 30
+config.audible_bell = 'Disabled'
+
+config.enable_kitty_keyboard = true
+
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = false
+config.tab_bar_at_bottom = true
+
+config.term = 'wezterm'
+config.window_padding = {
+  bottom = '5pt',
+  top = '5pt',
+}
 
 return config
