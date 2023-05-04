be21cf94975eb4b57f814839920c77a2ee0fb929
Pierrick Guillaume <pguillaume@sequans.com>
2023-05-04 15:07:28 +0200

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,31 +3,48 @@ if status is-interactive
   fish_vi_key_bindings
   fish_vi_cursor
   fzf_key_bindings
-  set nvm_default_version latest
+
   set fish_greeting
+
+  if test (hostname) = 'minibian' || test (hostname) = 'pandora'
+    set -gx GL_HOST gitlab-shared.sequans.com
+  end
+  # Add command abbreviation
+
+  abbr -a dc docker compose
+  abbr -a ls exa -lh
+  abbr -a vim nvim
+  abbr -a cm chezmoi
+  abbr -a gl glab
+
+  abbr -a g git
+  abbr -a ga git add
+  abbr -a gc git commit
+
+  abbr -a tm tmux
+  abbr -a ta tmux attach -t
+
+  set nvm_default_version latest
+
 else
+
   # Used for GPG asking for password using curses
   set -gx GPG_TTY (tty)
+
+  set -gx EDITOR nvim
+  set -gx MANPAGER 'nvim +Man!'
+  set -gx TERMINAL 'wezterm'
+
+  set -gx QT_STYLE_OVERRIDE kvantum
+  # set QT_QPA_PLATFORMTHEME=qt5ct
+
+  setxkbmap -option caps:escape
+
+  fish_add_path --global "$HOME/.local/bin"
+  fish_add_path --global "$HOME/.config/cargo/bin"
+
+  if test (hostname) = 'pandora'
+    fish_add_path --global "/opt/soft/toolchains64/aarch64-none-linux-gnu/bin/"
+  end
 end
 
-# Add command abbreviation
-
-abbr -a dc docker compose
-abbr -a ls exa -lh
-abbr -a vim nvim
-abbr -a cm chezmoi
-abbr -a gl glab
-
-abbr -a g git
-abbr -a ga git add
-abbr -a gc git commit
-
-abbr -a tm tmux
-abbr -a ta tmux attach -t
-
-if test (hostname) = 'minibian' || test (hostname) = 'pandora'
-  set -gx GL_HOST gitlab-shared.sequans.com
-end
-
-fish_add_path --append "$HOME/.local/bin"
-fish_add_path --append "$HOME/.config/cargo/bin"
