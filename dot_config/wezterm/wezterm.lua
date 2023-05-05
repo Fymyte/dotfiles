3981dde951f3f14d3b892f7724782629ad86441d
Pierrick Guillaume <pguillaume@sequans.com>
2023-05-05 10:49:42 +0200

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,13 +1,13 @@
-local wt = require 'wezterm'
+local wt = require("wezterm")
 
 local config = wt.config_builder()
 
-config.color_scheme = 'Catppuccin Mocha'
-config.font = wt.font 'Cascadia Code'
+config.color_scheme = "Catppuccin Mocha"
+config.font = wt.font("Cascadia Code")
 config.font_size = 13
 config.alternate_buffer_wheel_scroll_speed = 2
 config.animation_fps = 1
-config.audible_bell = 'Disabled'
+config.audible_bell = "Disabled"
 
 config.enable_kitty_keyboard = true
 
@@ -15,10 +15,12 @@ config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = false
 config.tab_bar_at_bottom = true
 
-config.term = 'wezterm'
+config.term = "wezterm"
 config.window_padding = {
-  bottom = '5pt',
-  top = '5pt',
+	bottom = "5pt",
+	top = "5pt",
 }
+
+config.leader = { key = "space", mods = "CTRL" }
 
 return config
