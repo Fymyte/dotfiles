a64acb8dc016da19477ddb478742280bbb4fe14c
Pierrick Guillaume <pguillaume@sequans.com>
2023-05-10 15:58:23 +0200

    Update .config/fish/config.fish Update .config/wezterm/wezterm.lua

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,17 +1,25 @@
+set fish_greeting
+
+if test hostname = 'minibian' || test hostname = 'pandora'
+  set -gx GL_HOST gitlab-shared.sequans.com
+  fish_add_path --global "/opt/soft/toolchains64/aarch64-none-linux-gnu/bin/"
+end
+
+fish_add_path --global "$HOME/.local/bin"
+fish_add_path --global "$HOME/.config/cargo/bin"
+
+set nvm_default_version latest
+
+set -gx EDITOR nvim
+set -gx MANPAGER 'nvim +Man!'
+set -gx TERMINAL 'wezterm'
+
 if status is-interactive
-    # Commands to run in interactive sessions can go here
   fish_vi_key_bindings
   fish_vi_cursor
   fzf_key_bindings
 
-  set fish_greeting
-
-  # set -l hostname (cat /etc/hostname)
-  if test hostname = 'minibian' || test hostname = 'pandora'
-    set -gx GL_HOST gitlab-shared.sequans.com
-  end
   # Add command abbreviation
-
   abbr -a dc docker compose
   abbr -a ls exa -lh
   abbr -a vim nvim
@@ -25,23 +33,11 @@ if status is-interactive
   abbr -a tm tmux
   abbr -a ta tmux attach -t
 
-  set nvm_default_version latest
-
-  set -gx EDITOR nvim
-  set -gx MANPAGER 'nvim +Man!'
-  set -gx TERMINAL 'wezterm'
 
   set -gx QT_STYLE_OVERRIDE kvantum
   # set QT_QPA_PLATFORMTHEME qt5ct
 
   setxkbmap -option caps:escape
-
-  fish_add_path --global "$HOME/.local/bin"
-  fish_add_path --global "$HOME/.config/cargo/bin"
-
-  if test hostname = 'pandora'
-    fish_add_path --global "/opt/soft/toolchains64/aarch64-none-linux-gnu/bin/"
-  end
 
 else
 
