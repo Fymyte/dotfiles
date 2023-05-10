139a8260867fc97fd4a76ceea0af9f118d4c44d0
Pierrick Guillaume <pguillaume@sequans.com>
2023-05-10 17:10:47 +0200

    fix: fish cursor

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -15,6 +15,11 @@ set -gx MANPAGER 'nvim +Man!'
 set -gx TERMINAL 'wezterm'
 
 if status is-interactive
+  set fish_cursor_default block
+  set fish_cursor_insert line
+  set fish_cursor_replace_one underscore
+  set fish_cursor_visual block
+  set fish_vi_force_cursor 1
   fish_vi_key_bindings
   fish_vi_cursor
   fzf_key_bindings
