9fbab17a3e1e025e1881a15a9607d0017c5d5452
Pierrick Guillaume <pguillaume@sequans.com>
2023-05-29 16:23:38 +0200

    Update .config/fish/conf.d/sequans.fish

--- dot_config/private_fish/conf.d/sequans.fish
+++ dot_config/private_fish/conf.d/sequans.fish
@@ -2,5 +2,5 @@ if test "$hostname" = 'minibian' || test "$hostname" = 'pandora'
   set -gx GL_HOST gitlab-shared.sequans.com
   fish_add_path --global "/opt/soft/toolchains64/aarch64-none-linux-gnu/bin/"
   fish_add_path "$HOME/.local/bin/cmake/bin"
-  fish_add_path "/opt/soft/toolchains64/aarch64-unknown-elf"
+  fish_add_path "/opt/soft/toolchains64/aarch64-unknown-elf/bin/"
 end
