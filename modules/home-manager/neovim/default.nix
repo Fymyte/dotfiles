39e616ea1fa1b80836db091126f6f91adcd5b4ed
Pierrick Guillaume <pguillaume@sequans.com>
2025-01-06 14:14:41 +0100

    add(programs): neovim: missing required lsp and formatters

--- modules/home-manager/neovim/default.nix
+++ modules/home-manager/neovim/default.nix
@@ -1,12 +1,26 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   home.packages = with pkgs.unstable; [
     # Neovim tools
     # TODO: move to separate file
     nixfmt-rfc-style
     yaml-language-server
+    lua-language-server
+    ltex-ls-plus
+    ruff
+    vale
     nixd
+    nil
     alejandra
+    kdlfmt
+    clang-tools
   ];
+
+  # Symlink old binary name to new ltex-ls-plus executable
+  home.file."${config.home.homeDirectory}/.local/bin/ltex-ls".source = "${pkgs.unstable.ltex-ls-plus}/bin/ltex-ls-plus";
 
   programs.neovim = {
     package = pkgs.unstable.neovim-unwrapped;
