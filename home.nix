4e468457a33bc127209799b191cc21b4f9aa8ce3
Pierrick Guillaume <pguillaume@sequans.com>
2025-02-03 13:21:58 +0100

    ref(nix): move config to separate file

--- home.nix
+++ home.nix
@@ -42,10 +42,6 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-  nix = {
-    package = pkgs.nix;
-    settings.experimental-features = ["nix-command" "flakes"];
-  };
 
   home.packages = [
     # Make sure GNU coreutils are always present
--- modules/home-manager/nix/default.nix
+++ modules/home-manager/nix/default.nix
@@ -1,7 +1,7 @@
-{ inputs, ... }:{
+{ inputs, pkgs, ... }:{
   nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
-  # nix = {
-  #   package = pkgs.nix;
-  #   settings.experimental-features = [ "nix-command" "flakes" ];
-  # };
+  nix = {
+    package = pkgs.nix;
+    settings.experimental-features = ["nix-command" "flakes"];
+  };
 }
