0c36e40b87ae7e8af1a18c1773c283adf90942a9
Pierrick Guillaume <pguillaume@fymyte.com>
2025-02-14 13:29:55 +0100

    ref(nix): explicitly include nixos and nix-community caches

--- modules/home-manager/nix/default.nix
+++ modules/home-manager/nix/default.nix
@@ -1,7 +1,14 @@
-{ inputs, pkgs, ... }:{
+{
+  inputs,
+  pkgs,
+  ...
+}: {
   nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
   nix = {
     package = pkgs.nix;
     settings.experimental-features = ["nix-command" "flakes"];
+
+    settings.substituters = ["https://cache.nixos.org/" "https://nix-community.org/cache/"];
+    settings.trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
   };
 }
