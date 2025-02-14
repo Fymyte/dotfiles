82d97fa99fd78ede713f502f60db648407494828
Pierrick Guillaume <pguillaume@fymyte.com>
2025-02-14 13:29:16 +0100

    ref(snorlax): properly set required env variables

--- hosts/snorlax/default.nix
+++ hosts/snorlax/default.nix
@@ -5,21 +5,18 @@
 }: {
   sops.defaultSopsFile = ../../secrets/sequans.yaml;
 
-  # home.username = "pguillaume";
-  # home.homeDirectory = "/data/exports/users/${config.home.username}";
-
-  # programs.git.userEmail = "${config.home.username}@sequans.com";
-
   # This is not a nixos system
   nixGL.packages = inputs.nixgl.packages;
 
   home.sessionPath = [
     "/opt/soft/toolchains64/nds32le-unknown-elf/bin"
     "${config.home.homeDirectory}/dev/sqn/sqn-sdk/tools/scriptkit/bash/"
+    "${config.home.homeDirectory}/dev/sqn/sqn-sdk/tools/scriptkit/python/bin/"
     "${config.home.homeDirectory}/x-tools/nds32le-linux/bin/"
   ];
 
   home.sessionVariables = {
     GL_HOST = "gitlab-shared.sequans.com";
+    PYTHONPATH = "$PYTHONPATH:${config.home.homeDirectory}/dev/sqn/sqn-sdk/tools/scriptkit/python";
   };
 }
