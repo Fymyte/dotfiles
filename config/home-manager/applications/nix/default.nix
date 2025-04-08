d1c69b4e8fb2361778d6caa35ab5607375762dca
Pierrick Guillaume <pguillaume@fymyte.com>
2025-04-08 10:05:33 +0200

    ref(nix): get github access token from sops secrets

--- config/home-manager/applications/nix/default.nix
+++ config/home-manager/applications/nix/default.nix
@@ -1,11 +1,21 @@
 {
+  config,
   inputs,
   pkgs,
   ...
 }: {
-  nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
+  sops.templates."nix-access-tokens.conf".content = ''
+    extra-access-tokens = github.com=${config.sops.placeholder.github-token}
+  '';
+
   nix = {
     package = pkgs.nix;
+    nixPath = ["nixpkgs=${inputs.nixpkgs}"];
     settings.experimental-features = ["nix-command" "flakes"];
+
+    extraOptions = ''
+      !include ${config.sops.templates."nix-access-tokens.conf".path}
+    '';
+    # settings.access-tokens = ["github.com=${secrets.github-token}"];
   };
 }
