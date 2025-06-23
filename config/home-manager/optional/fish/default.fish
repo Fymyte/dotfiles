cb508351bfbd3071c20364ff976c1d2ef98f3311
Pierrick Guillaume <pguillaume@fymyte.com>
2025-06-23 13:18:27 +0200

    fix(fish): force add nix binaries in PATH

--- config/home-manager/optional/fish/default.fish
+++ config/home-manager/optional/fish/default.fish
@@ -2,3 +2,6 @@ not status is-interactive; and begin
     # Use curse input helper when using non-interractive command-line
     set -gx GPG_TTY (tty)
 end
+
+# Force the presence of nix profile binaries in PATH
+fish_add_path --path /nix/var/nix/profiles/default/bin
