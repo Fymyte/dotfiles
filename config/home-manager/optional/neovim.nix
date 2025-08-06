4a1c9e59940bdf0b1c4ec3b80683bdd34ebb4bbf
Pierrick Guillaume <pguillaume@fymyte.com>
2025-08-06 14:15:13 +0200

    fix(neovim): don't override system python interpretter

--- config/home-manager/optional/neovim.nix
+++ config/home-manager/optional/neovim.nix
@@ -1,43 +1,47 @@
+{ pkgs, ... }:
 {
-  pkgs,
-  config,
-  ...
-}: {
   stylix.targets.neovim.enable = false;
 
-  home.packages = with pkgs.unstable; [
-    tree-sitter
-    # Neovim tools
-    # TODO: move to separate file
-    nixfmt-rfc-style
-    yaml-language-server
-    lua-language-server
-    ruff
-    vale
-    nixd
-    nil
-    alejandra
-    kdlfmt
-    clang-tools
-    stylua
-    selene
-    taplo
-
-    harper
-
-    (pkgs.python3.withPackages (p: (with p; [
-      python-lsp-server
-      python-lsp-server.optional-dependencies
-      pylsp-mypy
-      pylsp-rope
-      pyls-isort
-    ])))
-    nodePackages.vscode-json-languageserver
+  home.packages = [
+    pkgs.ruff
   ];
 
   programs.neovim = {
     package = pkgs.unstable.neovim-unwrapped;
     enable = true;
     defaultEditor = true;
+
+    extraPackages = with pkgs.unstable; [
+      tree-sitter
+      # Neovim tools
+      # TODO: move to separate file
+      nixfmt-rfc-style
+      yaml-language-server
+      lua-language-server
+      vale
+      nixd
+      nil
+      alejandra
+      kdlfmt
+      clang-tools
+      stylua
+      selene
+      taplo
+
+      harper
+
+      nodePackages.vscode-json-languageserver
+
+      (pkgs.python3.withPackages (
+        p:
+        (with p; [
+          python-lsp-server
+          python-lsp-server.optional-dependencies
+          pylsp-mypy
+          pylsp-rope
+          pyls-isort
+        ])
+      ))
+    ];
   };
 }
