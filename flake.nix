f86bd6537e10a0923c909567649e66019405f285
Pierrick Guillaume <pguillaume@fymyte.com>
2025-10-29 10:04:32 +0100

    fix: force nixos-hardware to branch master

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    hardware.url = "github:nixos/nixos-hardware";
+    hardware.url = "github:nixos/nixos-hardware/master";
 
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
