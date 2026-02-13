486015d3664311eba2c04f89c667a336d516e8cd
Pierrick Guillaume <pguillaume@fymyte.com>
2026-02-13 11:28:57 +0100

    fix(pipoupc): use zellij instead of tmux
    Replace tmux with zellij and use global zellij config

--- home/fymyte/pipoupc/default.nix
+++ home/fymyte/pipoupc/default.nix
@@ -15,7 +15,7 @@
       "config/home-manager/optional/bash.nix"
       "config/home-manager/optional/ghostty"
       "config/home-manager/optional/wezterm"
-      "config/home-manager/optional/tmux"
+      "config/home-manager/optional/zellij"
       "config/home-manager/optional/neovim.nix"
       "config/home-manager/optional/page.nix"
       "config/home-manager/optional/walker.nix"
