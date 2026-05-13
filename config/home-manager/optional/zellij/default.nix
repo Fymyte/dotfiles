9ff8f2d1229b57a23d336547c7d53a7cd9eb856d
Pierrick Guillaume <pguillaume@fymyte.com>
2026-05-13 11:36:53 +0200

    fix(zellij): use fixed zellij theme
    The default theme generated by stylix was breaking with alternate tabs.
    Use fixed one for now

--- config/home-manager/optional/zellij/default.nix
+++ config/home-manager/optional/zellij/default.nix
@@ -1,6 +1,8 @@
 {...}: {
   xdg.configFile."zellij/config.kdl".source = ./config.kdl;
-  stylix.targets.zellij.enable = true;
+  xdg.configFile."zellij/themes/custom-catppuccin-mocha-peach.kdl".source = ./theme.kdl;
+  # Disable stylix and use my own fixed theme for now
+  stylix.targets.zellij.enable = false;
   programs.zellij = {
     enable = true;
   };
